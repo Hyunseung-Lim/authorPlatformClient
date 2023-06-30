71c3a820a790bf86f33bf822774dfd3c5b696bb8
Hyunseung Lim <charlie9807@kaist.ac.kr>
2023-06-30 18:00:59 +0900

    author platform v1.1

---
+++ public/src/Components/Questionbox/questionbox.js
@@ -0,0 +1,123 @@
+import React, { useState, useRef, useEffect} from 'react'
+import './questionbox.css'
+import TextareaAutosize from 'react-autosize-textarea';
+import ToggleButton from 'react-toggle-button';
+
+
+export const Questionbox = (props) => {
+    const [isEdit, setIsEdit] = useState(false);
+    const [isPublic, setIsPublic] = useState(false);
+    const [isEditOrder, setIsEditOrder] = useState(false);
+    const [prevAnswer, setPrevAnswer] = useState("");
+    const [currentAnswer, setCurrentAnser] = useState("");
+    const textAreaRef = useRef(null);
+
+    useEffect(() => {
+        setPrevAnswer(props.answer);
+        setCurrentAnser(props.answer);
+        setIsPublic(props.isPublic);
+        setIsEditOrder(props.isEditOrder);
+        if(props.isEditOrder === false) {
+            cancelAnswer();
+        }
+    }, [])
+
+    useEffect(() => {
+        if(textAreaRef.current) {
+            textAreaRef.current.dispatchEvent(new Event('input', { bubbles: true }));
+        }
+    }, [currentAnswer]);
+
+    useEffect(()=> {
+        setIsEditOrder(props.isEditOrder);
+        if(props.isEditOrder === false) {
+            cancelAnswer();
+        }
+    }, [props.isEditOrder]);
+
+    const currentAnswerHandler = (e) => {
+        e.preventDefault();
+        setCurrentAnser(e.target.value);
+    }
+
+    function changeIsEdit() {
+        setCurrentAnser(props.answer);
+        setIsEdit(true);
+        if(textAreaRef.current) {
+            textAreaRef.current.focus();
+        }
+    }
+
+    function cancelAnswer() {
+        setCurrentAnser(prevAnswer);
+        setIsEdit(false);
+    }
+
+    function changeAnswer() {
+        props.updateAnswer(currentAnswer);
+        setPrevAnswer(currentAnswer);
+        setIsEdit(false);
+    }
+
+    function changePublic(isPublic) {
+        props.updatePublic(!isPublic);
+        setIsPublic(!isPublic);
+    }
+
+    return(
+        <>
+            <div className='questionbox'>
+                <div className='contatiner'>
+                    <div className='questionbar'>
+                        <div className='question'>
+                            {props.question}
+                        </div>
+                        {isEditOrder ? <div className='buttons'>
+                            <div className='makepublic'>
+                                make public
+                            </div>
+                            <ToggleButton
+                                inactiveLabel={''}
+                                activeLabel={''}
+                                colors={{
+                                    activeThumb: {
+                                        base: 'rgb(164,44,37)'
+                                    },
+                                    inactiveThumb: {
+                                        base: 'rgb(190,154,152)'
+                                    },
+                                    active: {
+                                        base: 'rgb(234,209,207)',
+                                        hover: 'rgb(242,217,215)'
+                                    },
+                                    inactive: {
+                                        base: 'rgb(234,209,207)',
+                                        hover: 'rgb(242,217,215)'
+                                    }
+                                }}
+                                value={ isPublic }
+                                onToggle={(value) => {
+                                    changePublic(value);
+                                }} 
+                                />
+                            <img src="images/trash.png" className='deleteBtn' onClick={props.deleteQuestion}/>
+                        </div> : null}
+                    </div>
+                    <div className='answerbox'>
+                        <div className='answer'>
+                            {isEdit ? <TextareaAutosize className='editAnswer' ref={textAreaRef} value={currentAnswer} onChange={currentAnswerHandler} onResize={(e) => {}}/> : <div>{props.answer}</div>}
+                        </div>
+                        {isEditOrder ?
+                            <div className='buttonContainer'>
+                                {isEdit ? <div className='editBtns'><button className='cancelBtn' onClick={cancelAnswer}>cancel</button> <button className='completeBtn' onClick={changeAnswer}>complete</button></div> : <button onClick={changeIsEdit}>Edit the Answer</button>}
+                            </div>
+                            : null
+                        }
+                    </div>
+                </div>
+                <img key={props.id} className='handle' src="images/handle.svg" alt="handle" {...props.handle}/>
+                {/* {isEditOrder ? null : <img className='handle' src="images/handle.png" alt="handle" />} */}
+            </div>
+        </>
+    )
+}
---
+++ public/src/Pages/MainPage.js
@@ -0,0 +1,209 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useLocation } from 'react-router-dom';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+import { Topbar } from '../Components/Topbar/topbar';
+import { Questionbox } from '../Components/Questionbox/questionbox';
+import { Recommendquestion } from '../Components/Recommendquestion/recommendquestion';
+import './page.css'
+
+
+// a little function to help us with reordering the result
+const reorder = (array, fromIndex, toIndex) => {
+    const elementToMove = array.splice(fromIndex, 1)[0];
+    array.splice(toIndex, 0, elementToMove);
+    return array;
+};
+
+export const MainPage = (props) => {
+
+    const API_DOMAIN = "https://lucydata.lgresearch.ai";
+
+    const location = useLocation();
+    const { url } = location.state;
+    const[title, setTitle] = useState("Title of Research Paper");
+    const[authors, setAuthors] = useState("");
+    const[currentQuestion, setCurrentQuestion] = useState("");
+    const[recommendQs, setRecommendQs] = useState([]);
+    const[loadRQs, setLoadRQs] = useState(false);
+    const[isEditOrder, setIsEditOrder] = useState(true);
+    const[QnAs, setQnAs] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const getApi = 'https://qna-restapi-dxpyj.run.goorm.site/getMeta/' + String(url).split('/').pop();
+                const result = await axios(getApi);
+                setTitle(String(result.data.meta[0]));
+                setAuthors(result.data.meta[2].join(", "));
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const currentQuestionHandler = (e) => {
+        e.preventDefault();
+        setCurrentQuestion(e.target.value);
+    }
+
+    const generateQuestion = async () => {
+        setLoadRQs(true);
+        try {
+            const getApi = 'https://qna-restapi-dxpyj.run.goorm.site/getQuestion/' + String(url).split('/').pop();
+            const result = await axios(getApi);
+            setRecommendQs(result.data.questions);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+        setLoadRQs(false);
+    }
+
+    async function addQuestion (question) {
+        const answer = "Answer is being generated...";
+
+        await axios.post(API_DOMAIN + "/get_lucy_answer", {
+            "question": question, 
+            "title": title
+        },
+        {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        },
+        { withCredentials: true }
+        )
+        .then((response) => {
+            const res =response.data;
+            console.log(res.lucy_answer);
+            setQnAs(prevData => [...prevData, {question: String(question), answer: String(res.lucy_answer), isPublic: false}]);
+        })        
+    }
+
+    function deleteQuestion (index) {
+        setQnAs(prevData => prevData.filter((item, i) => i !== index));
+    }
+
+    function addRecommendQuestion (question, index) {
+        addQuestion(question);
+        setRecommendQs(prevData => {
+            let newArray = [...prevData];
+            newArray.splice(index, 1);
+            return newArray;
+        })
+    }
+
+    function addCurrentQuestion () {
+        if(currentQuestion !== "") {
+            addQuestion(currentQuestion);
+            setCurrentQuestion("");
+        }
+    }
+
+    function updateAnswer (updateIndex, newAnswer) {
+        setQnAs(prevData => (
+            prevData.map((QnA, index) => updateIndex === index ? {...QnA, answer: newAnswer } : QnA)
+        ));
+    }
+
+    function updatePublic (updateIndex, isPublic) {
+        setQnAs(prevData => (
+            prevData.map((QnA, index) => updateIndex === index ? {...QnA, isPublic: isPublic } : QnA)
+        ));
+        console.log(QnAs); 
+    }
+
+    function changeIsEditOrder () {
+        setIsEditOrder(!isEditOrder);
+        console.log(isEditOrder);
+    }
+
+
+    function onDragEnd(result) {
+        // dropped outside the list
+        if (!result.destination) {
+            return;
+        }
+        console.log("onDargEnd:", QnAs);
+        const newQnAs = reorder(
+            QnAs,
+            result.source.index,
+            result.destination.index
+        );
+        console.log(newQnAs);
+        setQnAs(newQnAs);
+    }
+
+    return(
+        <>
+            <div className='mainPage'>
+                <Topbar/>
+                <div className='container'>
+                    <div className='title'>
+                        {title}
+                    </div>
+                    <div className='authors'>
+                        {authors}
+                    </div>
+                    <div className='previewContainer'>
+                        <button className='previewBtn'>
+                            Preview
+                        </button>
+                    </div>
+                    <div className='subtitle'>
+                        Add Question
+                    </div>
+                    <div className='inputContainer'>
+                        <input value={currentQuestion} onChange={currentQuestionHandler} placeholder='Type Your Own Question'></input>
+                        <button onClick={addCurrentQuestion}>Add +</button>
+                    </div>
+                    <div className='subtitle'>
+                        Question Recommendation
+                    </div>
+                    <div className='recommendContainer'>
+                        <button className='recommendBtn' disabled={loadRQs} onClick={generateQuestion}>Recommend Question</button>
+                        {loadRQs ? <img className='loading' src="images/loading.gif" alt="loading" /> : recommendQs.map((rQ, index) => (<Recommendquestion question={rQ} addRecommendQuestion={() => addRecommendQuestion(rQ, index)}/>))}
+                    </div>
+                    <div className='subtitle'>
+                        QnA
+                        {/* <button className='editOrderBtn' onClick={changeIsEditOrder}>
+                            ⇅ Edit Order
+                        </button> */}
+                    </div>
+                    <DragDropContext className='questionContainer' onDragEnd={onDragEnd}>
+                        { QnAs.length === 0 ? <div className='noQuestion'>No Question</div> 
+                            :
+                            <Droppable droppableId="droppable">
+                                {(provided, snapshot) => (
+                                    <div
+                                        {...provided.droppableProps}
+                                        ref={provided.innerRef}
+                                        // style={getListStyle(snapshot.isDraggingOver)}
+                                    >
+                                    {QnAs.map((QnA, index) => (
+                                        <Draggable key={String(QnA.question + index)} draggableId={String(QnA.question + index)} index={index}>
+                                            {(provided, snapshot) => (
+                                                <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                >
+                                                    <Questionbox key={index} id={String(QnA.question + index)} question={QnA.question} answer={QnA.answer} isPublic={QnA.isPublic} isEditOrder={isEditOrder} updateAnswer={(newAnswer)=>updateAnswer(index, newAnswer)} updatePublic={(isPublic)=>updatePublic(index, isPublic)} deleteQuestion={() => deleteQuestion(index)} handle={provided.dragHandleProps}/>
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    {provided.placeholder}
+                                    </div>
+                                )}
+                            </Droppable>
+                        }
+                    </DragDropContext>
+                </div>
+                <div className='footer'>
+                </div>
+            </div>
+        </>
+    )
+}
